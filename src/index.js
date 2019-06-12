076a602888a9c40638dd789bea44e690ecf09d56
componey-daiwenqi <361904805@qq.com>
2019-06-12 20:15:55 +0800

    '编辑scss，自动厂商前缀'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import picurl from './images/picture.png';
+import './index.scss';
 
+// 插入图片
 const oImage = new Image();
 oImage.src = picurl;
+
+// 使用css
+oImage.classList.add('size');
 
 const eleRoot = document.querySelector('#root');
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,21 @@ module.exports = {
     module:{
         rules:[{
             test:/\.png$/,
-            use:'file-loader'
+            use:[{
+                loader:'url-loader',
+                options:{
+                    name:'[name]-[hash].[ext]', // 打包后的名字 placeholder
+                    outputPath: 'images',
+                    limit: 10240 // url-loader 当图片小于10kb的时候，使用base64
+                    // publicPath: 'assets/' 使用后assets/picture.png，发布后的路径
+                }
+            }]
+        },{
+            test: /\.css$/,
+            use:['style-loader','css-loader']
+        },{
+            test: /\.scss$/,
+            use:['style-loader','css-loader','sass-loader','postcss-loader']
         }]
     },
     output:{
